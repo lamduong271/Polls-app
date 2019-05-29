b3d2b2fd02b10e92e02b08357691782b64264e90
Lam Duong <lamdu@Lams-MacBook-Pro.local>
2019-05-30 01:04:42 +0300

    question detail

--- polls/urls.py
+++ polls/urls.py
@@ -2,5 +2,7 @@
 from django.urls import path
 from . import views
 urlpatterns = [
+    path('detail/<int:question_id>', views.detailView, name="detail-view"),
     path('', views.index),
+    path('questions/', views.viewList, name="questions-list"),
 ]
--- polls/views.py
+++ polls/views.py
@@ -1,8 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
+from .models import Question
 # Create your views here.
 def index(request):
     name = "Lam Duong"
     favorite = ["AB6ix","Wanna One", "Twice", "Black Pink"]
     context = {"name":name, "kpop": favorite }
     return render(request, "polls/index.html", context)
+
+def viewList(request):
+    questionList = Question.objects.all()
+    context = {"questions": questionList}
+    return render(request, 'polls/question-list.html', context)
+
+def detailView(request, question_id):
+    question = Question.objects.get(pk=question_id)
+    print(question)
+    context = {"detailQuestion":question}
+    return render(request,'polls/detail-question.html', context)
+
